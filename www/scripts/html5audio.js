ac6b0191efd213b7070911e7bbacba498e89a3a8
Tomáš Taro <tomas@getadblock.com>
2014-11-08 00:39:49 +0100

    Added Europa 2 station

--- www/scripts/html5audio.js
+++ www/scripts/html5audio.js
@@ -20,10 +20,10 @@ var expres = {
 var activityIndicator  = document.getElementById('activityindicator');
 var actual_cover_url   = null;
 var getMetadata        = null;         // Function - setInterval() fn, which calls get_song(radio) for updating name of song
-var isPlaying          = false;          // Bool - If stream is playing, it's given value is true
-var readyStateInterval = null;  // Function - setInterval() fn, see line 63 for more info
-var stream             = null;              // Object - HTML5 Audio object
-var streamURL          = null;           // String - URL of chosen stream
+var isPlaying          = false;        // Bool - If stream is playing, it's given value is true
+var readyStateInterval = null;         // Function - setInterval() fn, see line 63 for more info
+var stream             = null;         // Object - HTML5 Audio object
+var streamURL          = null;         // String - URL of chosen stream
 var station            = null;
 
 // HTML5audio object
@@ -45,6 +45,9 @@ var html5audio = {
                 break;
             case "funradio":
                 streamURL = "http://stream.funradio.sk:8000/fun128.mp3";
+                break;
+            case "europa2":
+                streamURL = "http://ice2.europa2.sk/fm-europa2sk-128";
                 break;
         }
         // Init chosen radio station
@@ -105,7 +108,7 @@ var html5audio = {
 // Parse stream track info
 var get_song = function(station) {
     function reqListener () {
-        if (station !== "slovensko") {
+        if (station !== "slovensko" && station !== "europa2") {
             var artist_song = null;
             if (station === "expres")
                 artist_song = this.responseText.split(":");
@@ -125,54 +128,7 @@ var get_song = function(station) {
 
             // LAST.FM API for getting cover of song
             if (artist_song[0] && artist_song[1]) {
-                $.ajax({
-                    type: "POST",
-                    url: "http://ws.audioscrobbler.com/2.0/?",
-                    data: { method: "track.getInfo", api_key: "cdfa596938a9f8083739ee5ee08e7e29",
-                           artist: artist_song[0], track: artist_song[1] },
-                    success: function(response) {
-
-                        if (response.querySelector("image")) {
-                            var cover_url = response.querySelector("image").textContent;
-                        } else {
-                            $("#artist").html(artist_song[0]).text();
-                            $("#song").html(artist_song[1]).text();
-                        }
-
-                        // We already got url of image -> it means that callback was successful,
-                        // so add image to playing div
-                        if (cover_url && actual_cover_url !== cover_url) {
-                            if (document.querySelector("#status > img")) {
-                                document.querySelector("#status > img").src = cover_url;
-
-                            } else {
-                                $('<img src="' + cover_url + '">').load(function() {
-                                    $(this).insertBefore('#playing').addClass("radioimg");
-                                });
-                            }
-                            actual_cover_url = cover_url;
-                        } else if (!cover_url) {
-                            if (!document.querySelector("#status > img")) {
-                                $('<img src="images/' + station + '.png">').load(function() {
-                                    $(this).insertBefore('#playing').addClass("radioimg");
-                                });
-                            } else {
-                                document.querySelector("#status > img").src = "images/" + station + ".png";
-                            }
-                            actual_cover_url = cover_url;
-                        } else {}
-                    },
-                    error: function(status) {
-                        // Replace image with station image, when error occurs
-                        if (!document.querySelector("#status > img")) {
-                            $('<img src="images/' + station + '.png">').load(function() {
-                                $(this).insertBefore('#playing').addClass("radioimg");
-                            });
-                        } else {
-                            document.querySelector("#status > img").src = "images/" + station +".png";
-                        }
-                    }
-                });
+                ajaxCover(artist_song[0], artist_song[1]);
             } else {
                 // If we don't know either name of artist or song, replace img with station img
                 if (!document.querySelector("#status > img")) {
@@ -184,27 +140,48 @@ var get_song = function(station) {
                 }
             }
         } else {
-            // Specific handling of Rádio Slovensko station
-            var html = document.implementation.createHTMLDocument('');
-            html.documentElement.innerHTML = this.responseText;
-            html.querySelector(".ro-slovensko > .playRadio > .overflow > strong").remove();
-            $("#song").text(function() {
-                var text = html.querySelector(".ro-slovensko > .playRadio > .overflow").textContent;
-                return text.replace(/-/,"").replace(" ","");
-            });
+            if (station === "slovensko") {
+                // Specific handling of Rádio Slovensko station
+                var html = document.implementation.createHTMLDocument('');
+                html.documentElement.innerHTML = this.responseText;
+                html.querySelector(".ro-slovensko > .playRadio > .overflow > strong").remove();
+                $("#song").text(function() {
+                    var text = html.querySelector(".ro-slovensko > .playRadio > .overflow").textContent;
+                    return text.replace(/-/,"").replace(" ","");
+                });
+
+                $("#artist").text("Rádio Slovensko");
+            } else {
+                $.get("http://rds.lagardere.cz/getRadio.php?station=okey", function(data) {
+                    var artist = data.querySelector("songArtist").textContent.
+                    replace(/^\s*\/\/<!\[CDATA\[([\s\S]*)\/\/\]\]>\s*\z/,"");
+                    var song = data.querySelector("songTitle").textContent.
+                    replace(/^\s*\/\/<!\[CDATA\[([\s\S]*)\/\/\]\]>\s*\z/,"");
+
+                    if (artist && song)
+                        ajaxCover(artist, song);
+
+                    $("#artist").text(function() {
+                        return artist;
+                    });
+
+                    $("#song").text(function() {
+                        return song;
+                    });
+                });
+            }
 
             if (!document.querySelector("#status > img")) {
-                $('<img src="images/slovensko.png">').load(function() {
+                $('<img src="images/' + station + '.png">').load(function() {
                     $(this).insertBefore('#playing').addClass("radioimg");
                 });
             } else {
-                document.querySelector("#status > img").src = "images/slovensko.png";
+                document.querySelector("#status > img").src = "images/" + station +".png";
             }
 
-            $("#artist").text("Rádio Slovensko");
+            if ($(".marquee").text().length > 20)
+                $(".marquee").marquee();
         }
-        if ($(".marquee").text().length > 20)
-            $(".marquee").marquee();
     }
 
     var url = "http://tomastaro.sk/parser" + station + ".php";
@@ -213,4 +190,55 @@ var get_song = function(station) {
     xhr.onload = reqListener;
     xhr.open("get", url, true);
     xhr.send();
+}
+
+function ajaxCover(artist, track) {
+    $.ajax({
+        type: "POST",
+        url: "http://ws.audioscrobbler.com/2.0/?",
+        data: { method: "track.getInfo", api_key: "cdfa596938a9f8083739ee5ee08e7e29",
+               artist: artist, track: track },
+        success: function(response) {
+
+            if (response.querySelector("image")) {
+                var cover_url = response.querySelector("image").textContent;
+            } else {
+                $("#artist").html(artist).text();
+                $("#song").html(track).text();
+            }
+
+            // We already got url of image -> it means that callback was successful,
+            // so add image to playing div
+            if (cover_url && actual_cover_url !== cover_url) {
+                if (document.querySelector("#status > img")) {
+                    document.querySelector("#status > img").src = cover_url;
+
+                } else {
+                    $('<img src="' + cover_url + '">').load(function() {
+                        $(this).insertBefore('#playing').addClass("radioimg");
+                    });
+                }
+                actual_cover_url = cover_url;
+            } else if (!cover_url) {
+                if (!document.querySelector("#status > img")) {
+                    $('<img src="images/' + station + '.png">').load(function() {
+                        $(this).insertBefore('#playing').addClass("radioimg");
+                    });
+                } else {
+                    document.querySelector("#status > img").src = "images/" + station + ".png";
+                }
+                actual_cover_url = cover_url;
+            } else {}
+        },
+        error: function(status) {
+            // Replace image with station image, when error occurs
+            if (!document.querySelector("#status > img")) {
+                $('<img src="images/' + station + '.png">').load(function() {
+                    $(this).insertBefore('#playing').addClass("radioimg");
+                });
+            } else {
+                document.querySelector("#status > img").src = "images/" + station +".png";
+            }
+        }
+    });
 }
